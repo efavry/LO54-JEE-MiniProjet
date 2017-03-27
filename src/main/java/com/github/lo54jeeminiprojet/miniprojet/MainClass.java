df74f1e60566b186078c9709c9c56c883498d77a
Erwan Favry <erwan.favry@cloudnetcare.com>
2017-03-27 20:46:14 +0100

    Add Maven Basic pom and mainclass

---
+++ src/main/java/com/github/lo54jeeminiprojet/miniprojet/MainClass.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Erwan Favry on 27/03/2017.
+ */
+package com.github.lo54jeeminiprojet.miniprojet;
+
+
+
+public class MainClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+
+
+}
