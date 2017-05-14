7daf143437bad3f4d6bd7300eb54b5ac6af02916
Notmoo-PC\Notmoo <guillaumeprost39@gmail.com>
2017-05-14 23:29:43 +0200

    PublisherService : ajout du corps des méthodes de gestion et de publish (generation de contenu de message exclu)

--- src/main/java/com/github/lo54jeeminiprojet/miniprojet/Service/PublisherService.java
+++ src/main/java/com/github/lo54jeeminiprojet/miniprojet/Service/PublisherService.java
@@ -2,21 +2,52 @@ package com.github.lo54jeeminiprojet.miniprojet.Service;
 
 import com.github.lo54jeeminiprojet.miniprojet.Entity.Client;
 import com.github.lo54jeeminiprojet.miniprojet.Repository.PublisherDao.IPublisherDao;
+import com.github.lo54jeeminiprojet.miniprojet.Repository.PublisherDao.JmsDao;
+import com.github.lo54jeeminiprojet.miniprojet.Repository.PublisherDao.exceptions.PublisherDaoException;
+import com.github.lo54jeeminiprojet.miniprojet.Service.exceptions.PublisherServiceException;
+
+import javax.jms.JMSException;
 
 public class PublisherService {
 
 	private IPublisherDao dao;
+	private int registreationTopicID;
 
-	public void createTopic(String title) {
+	public PublisherService(String url) throws PublisherServiceException {
+		try {
+			dao = new JmsDao(url);
+			registreationTopicID = dao.addTopic("LO54_JEE-JMS_PROJECT");
+		}catch(JMSException e){
+			throw new PublisherServiceException("Error during initialization", e);
+		}catch(PublisherDaoException e){
+			throw new PublisherServiceException("Error during initialization", e);
+		}
+	}
 
+	public void startService() {
+		dao.startConnection();
 	}
 
 	public void stopService() {
-
+		dao.stopConnection();
 	}
 
-	public void publishRegistrationMessage(Client client) {
-
+	public void closeService() {
+		dao.closeConnection();
 	}
 
+	public boolean publishRegistrationMessage(Client client) {
+		try {
+			dao.publishText(registreationTopicID, generateRegistrationMessage(client));
+			return true;
+		} catch (PublisherDaoException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	private String generateRegistrationMessage(Client client){
+		//TODO
+		return null;
+	}
 }
