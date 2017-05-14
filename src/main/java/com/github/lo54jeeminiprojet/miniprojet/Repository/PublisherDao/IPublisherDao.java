0905497ad54be03fc5ed56552d6f6efdd91dee14
Notmoo-PC\Notmoo <guillaumeprost39@gmail.com>
2017-05-14 23:28:51 +0200

    JmsDao : écriture corps méthodes + ajout méthodes publiques à l'interface IPublisherDao

--- src/main/java/com/github/lo54jeeminiprojet/miniprojet/Repository/PublisherDao/IPublisherDao.java
+++ src/main/java/com/github/lo54jeeminiprojet/miniprojet/Repository/PublisherDao/IPublisherDao.java
@@ -1,8 +1,18 @@
 package com.github.lo54jeeminiprojet.miniprojet.Repository.PublisherDao;
 
 import com.github.lo54jeeminiprojet.miniprojet.Entity.Client;
+import com.github.lo54jeeminiprojet.miniprojet.Repository.PublisherDao.exceptions.PublisherDaoException;
+
+import java.io.Serializable;
 
 public interface IPublisherDao {
 
-    public void publishRegistration(Client client);
+    public int addTopic(String title) throws IndexOutOfBoundsException, PublisherDaoException;
+    public boolean removeTopic(int topicID) throws PublisherDaoException;
+    public boolean publishText(int topicID, String text) throws PublisherDaoException;
+    public boolean publishObject(int topicID, Serializable obj) throws PublisherDaoException;
+    public boolean startConnection();
+    public boolean stopConnection();
+    public boolean closeConnection();
+
 }
--- src/main/java/com/github/lo54jeeminiprojet/miniprojet/Repository/PublisherDao/JmsDao.java
+++ src/main/java/com/github/lo54jeeminiprojet/miniprojet/Repository/PublisherDao/JmsDao.java
@@ -1,11 +1,136 @@
 package com.github.lo54jeeminiprojet.miniprojet.Repository.PublisherDao;
 
-import com.github.lo54jeeminiprojet.miniprojet.Entity.Client;
+import com.github.lo54jeeminiprojet.miniprojet.Repository.PublisherDao.exceptions.PublisherDaoException;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JmsDao implements IPublisherDao {
-    
+
+    private int topicIdCounter;
+    private TopicConnectionFactory connectionFactory;
+    private TopicConnection connection;
+    private TopicSession session;
+
+    private Map<Integer, Topic> topics;
+
+    public JmsDao(String url) throws JMSException {
+        if(url==null || url.isEmpty())
+            throw new IllegalArgumentException("Empty url is not allowed");
+
+        topicIdCounter = 1;
+        connectionFactory = new ActiveMQConnectionFactory(url);
+        connection = connectionFactory.createTopicConnection();
+
+        topics = new HashMap<>();
+    }
+
     @Override
-    public void publishRegistration (final Client client) {
-        
+    public int addTopic(String title) throws PublisherDaoException{
+        if(session!=null && title!= null && !title.isEmpty()) {
+
+            if(topicIdCounter==Integer.MAX_VALUE)
+                throw new IndexOutOfBoundsException("Too many topics registered.");
+
+            try {
+                Topic topic = session.createTopic(title);
+                topics.put(topicIdCounter, topic);
+            } catch (JMSException e) {
+                throw new PublisherDaoException(e);
+            }
+
+            topicIdCounter++;
+            return topicIdCounter-1;
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean removeTopic(int topicID) throws PublisherDaoException {
+        if(topics.containsKey(topicID)){
+            topics.remove(topicID);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean publishText(int topicID, String text) throws PublisherDaoException{
+        if(session == null)
+            return false;
+
+        try {
+            TextMessage msg = session.createTextMessage();
+            msg.setText(text);
+             return publishMessage(topicID, msg);
+        } catch (JMSException e) {
+            throw new PublisherDaoException(e);
+        }
+    }
+
+    @Override
+    public boolean publishObject(int topicID, Serializable obj) throws PublisherDaoException {
+        if(session == null)
+            return false;
+
+        try {
+            ObjectMessage msg = session.createObjectMessage();
+            msg.setObject(obj);
+            return publishMessage(topicID, msg);
+        } catch (JMSException e) {
+            throw new PublisherDaoException(e);
+        }
+    }
+
+    private boolean publishMessage(int topicID, Message message) throws PublisherDaoException {
+        if(topics.containsKey(topicID) && topics.get(topicID)!=null){
+            try {
+                TopicPublisher publisher = session.createPublisher(topics.get(topicID));
+                publisher.publish(message);
+                return true;
+            } catch (JMSException e) {
+                throw new PublisherDaoException(e);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean startConnection() {
+        try {
+            connection.start();
+            session = connection.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+            return true;
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean stopConnection() {
+        try {
+            session.close();
+            connection.stop();
+            return true;
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean closeConnection() {
+        try {
+            session.close();
+            connection.close();
+            return true;
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
