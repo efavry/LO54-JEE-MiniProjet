2fa027b6dd5c45aceca7293b61b23628e78a65bf
Notmoo-PC\Notmoo <guillaumeprost39@gmail.com>
2017-06-02 20:27:18 +0200

    Ajout clss servlets

---
+++ app/src/main/java/servlet/RegistrationFailureServlet.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Notmoo on 02/06/2017.
+ */
+@WebServlet(name = "RegistrationFailureServlet", urlPatterns="/registration/fail")
+public class RegistrationFailureServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ app/src/main/java/servlet/RegistrationSuccessServlet.java
@@ -0,0 +1,18 @@
+package servlet;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+/**
+ * Created by Notmoo on 02/06/2017.
+ */
+@WebServlet(name = "RegistrationSuccessServlet", urlPatterns="/registration/success")
+public class RegistrationSuccessServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
